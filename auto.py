eaf3aca87863e243ac799f439966351cbdd09c1a
zhouqz <475506780@qq.com>
2021-08-09 00:20:28 +0800

    参数化 固化配置

--- auto.py
+++ auto.py
@@ -8,23 +8,35 @@ from pynput.mouse import Listener
 from util import on_move, on_scroll
 
 
-# sizex,sizey=pyautogui.size()
-# print(sizex,sizey)
-# pyautogui.moveTo(sizex/2,sizey/2,duration=1)
-# pyautogui.moveTo(0,0,duration=1)  # 左上角
-# pyautogui.moveTo(sizex - 1,sizey - 1,duration=1)  # 右下角
-# pyautogui.moveTo(0,0,duration=1)  # 左上角
-# pyautogui.moveTo(0,0,duration=1)  # 左上角
-# pyautogui.moveTo(1921,0,duration=1) # 外置显示器左上角
-# pyautogui.moveTo(1921,0,duration=1) # 外置显示器左上角
-# pyautogui.moveRel(100, -200, duration=0.5)
-
 STARTWAIT = 0  # 开始时可能还在占领倒计时，不能直接开始
 time.sleep(STARTWAIT)
+# 攻击按钮与点击位置的偏移量
+ATTACKBUTTONX = 120
+ATTACKBUTTONY = -74
+# 选将  默认选择第三个
+SELECTX = 2387
+SELECTY = 475
+# 攻击确认按钮位置
+ATTACKCONFIMX = 2643
+ATTACKCONFIMY = 379
+POSITIONFIXX = 2385
+POSITIONFIXY = 232
+
+
+# 攻击取消  两步操作
+ATTACKCANCELX = 2714
+ATTACKCANCELY = 101
+ATTACKBACKX = 2790
+ATTACKBACKY = 63
+
+DEBUG = True
 
 
 def getsleeptime():
-    return 700
+    if DEBUG:
+        return 1
+    # TODO 根据当前时间自行计算等待时间。    规则是  占领时间+ 行军时间  默认假设行军时间在100s内
+    return 400
 
 
 def getpositions():
@@ -49,19 +61,17 @@ def getpositions():
 def atteck():
     ...
     pyautogui.click()
-    pyautogui.moveRel(246, -105, duration=0.5)
+    pyautogui.moveRel(ATTACKBUTTONX, ATTACKBUTTONY, duration=0.5)
     pyautogui.click()
-    pyautogui.moveTo(2484, 561, duration=1)
+    pyautogui.moveTo(SELECTX, SELECTY, duration=1)
     pyautogui.click()
-
-    # 取消  方便调试
-    # pyautogui.click(2923, 115, duration=0.5)
-    # pyautogui.click(3002, 70, duration=0.5)
-    # 真打
-    pyautogui.click(2830, 477, duration=0.5)
-
-    # pyautogui.moveTo(3004, 579, duration=1)
-    # pyautogui.click()
+    if DEBUG:
+        # 取消  方便调试
+        pyautogui.click(ATTACKCANCELX, ATTACKCANCELY, duration=0.5)
+        pyautogui.click(ATTACKBACKX, ATTACKBACKY, duration=0.5)
+    else:
+        # 真打
+        pyautogui.click(ATTACKCONFIMX, ATTACKCONFIMY, duration=0.5)
 
 
 def pointpos(positions=[(2317, 533)]):
@@ -71,8 +81,8 @@ def pointpos(positions=[(2317, 533)]):
         if not lastpos:
             pyautogui.moveTo(item[0], item[1], duration=1)
         else:
-            pyautogui.moveTo(item[0]-lastpos[0]+2497,
-                             item[1]-lastpos[1]+288, duration=1)
+            pyautogui.moveTo(item[0]-lastpos[0]+POSITIONFIXX,
+                             item[1]-lastpos[1]+POSITIONFIXY, duration=1)
         lastpos = item
         atteck()
         time.sleep(getsleeptime())
---
+++ windowsize.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   windowsize.py
+@Time    :   2021/08/08 20:30:14
+@Author  :   shoujun.li
+@Version :   1.0
+@Contact :   highgogo@pm.me
+'''
+# import win32gui
+# import win32api
